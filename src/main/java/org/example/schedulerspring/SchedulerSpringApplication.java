43c35bf4e33bc3bc8d3e524e9d6b08c00e330c6a
Soong0812 <shotpo90@gmail.com>
2025-07-31 21:26:53 +0900

    first commit

---
+++ src/main/java/org/example/schedulerspring/SchedulerSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.schedulerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerSpringApplication.class, args);
+    }
+
+}
